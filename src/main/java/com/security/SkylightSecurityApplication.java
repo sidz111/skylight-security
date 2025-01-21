c6f7025794771b76ea3a90b04bedd2ae71614c07
sidz111 <sssurwade2212@gmail.com>
2025-01-21 15:26:25 +0530

    first commit

---
+++ src/main/java/com/security/SkylightSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylightSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkylightSecurityApplication.class, args);
+	}
+
+}
